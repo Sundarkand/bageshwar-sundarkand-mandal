e03780fb8302c6ca95dcfbe6d15b8f6356d5279e
Sundarkand <fforbit8@gmail.com>
2026-03-02 00:26:28 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -12,13 +12,14 @@ import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-s
 //  REPLACE these values with your Firebase project
 //  Go to: Firebase Console > Project Settings > Web App
 // -----------------------------------------------
+// Import the functions you need from the SDKs you need
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyCEkcXfisIO7a_b0OHre8iFA7sEQMU0Q-A",
+  authDomain: "bageshwar-mandal.firebaseapp.com",
+  projectId: "bageshwar-mandal",
+  storageBucket: "bageshwar-mandal.firebasestorage.app",
+  messagingSenderId: "257893299378",
+  appId: "1:257893299378:web:3eaba9d84f9b6a8c78c5ab"
 };
 
 // Initialize Firebase App
